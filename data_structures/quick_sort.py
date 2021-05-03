c9f105ebef000e21bb03396b9061b968625f44ac
jmstudyacc <jmstudyacc@gmail.com>
2021-05-03 15:24:20 +0100

    adding a quick sort algorithm

--- data_structures/quick_sort.py
+++ data_structures/quick_sort.py
@@ -2,28 +2,28 @@
 # this version will pick the 'middle' element of the 'array'
 # quicksort requires a partition function as well as the sort function
 import random
-
+import time
 
 def quicksort(arr, left, right):
-    print(f"Current left value: {left}")
-    print(f"Current right value: {right}")
+    # print(f"Current left value: {left}")
+    # print(f"Current right value: {right}")
     # base case to exit the recursive lookup
     if left >= right:
         return
 
-    pivot = arr[(left + right) // 2]
-    print(f"Current pivot value: {pivot}")
+    pivot = arr[(left + (right - left) // 2)]
+    # print(f"Current pivot value: {pivot}")
 
     # partition array around this pivot point - value of left is updated with each recursion
     index = partition(arr, left, right, pivot)
-    print(f"Current index value: {index}")
+    # print(f"Current index value: {index}")
 
-    print(f"Current list during Quicksort: {arr}\n")
+    # print(f"Current list during Quicksort: {arr}\n")
     # recursive quicksort call on the array with the lower (left) and upper (mid -1)
     quicksort(arr, left, index - 1)
 
     # recursive quicksort call on the array with the index (pivot) and end of array (len - 1)
-    quicksort(arr, index + 1, right)
+    quicksort(arr, index, right)
 
 
 def partition(arr, left, right, pivot):
@@ -50,15 +50,12 @@ def partition(arr, left, right, pivot):
 
 if __name__ == "__main__":
 
-    arr2 = []
-
-    for i in range(10):
-        num = random.randrange(0, 101)
-        if num in arr2:
-            continue
-        else:
-            arr2.append(num)
-
-    print(f"The list before Quicksort is applied: {arr2}")
-    quicksort(arr2, 0, len(arr2) - 1)
-    print(f"The list after Quicksort is applied: {arr2}")
+    arr2 = [32, 73, 71, 78, 93, 30, 0, 53, 1, 74]
+    arr1 = []
+    for i in range(2500000):
+        arr1.append(random.randrange(0, 5000000))
+    start_time = time.time()
+    # print(f"The list before Quicksort is applied: {arr1}")
+    quicksort(arr1, 0, len(arr1) - 1)
+    # print(f"The list after Quicksort is applied: {arr1}")
+    print(f"Program took {time.time() - start_time} seconds. ")
